4427f6883afe34e052b37a29c7ad750a56b3ec1b
jiita <zknsftk1msrk@gmail.com>
2016-09-04 21:47:06 +0900

    move files

--- src/LPC4370_sdr.cpp
+++ src/LPC4370_sdr.cpp
@@ -22,30 +22,11 @@
 #include <stdio.h>
 
 #include "lpc43xx_gpio.h"
-
 #include "lpc43xx_cgu.h"
 
 
+#include "driver/timer.h"
 
-
-//  SysTick_Handler - just increment SysTick counter
-volatile uint32_t msTicks; // counter for 1ms SysTicks
-
-extern "C" {
-void SysTick_Handler(void) {
-    msTicks++;
-}
-}
-
-// ****************
-// systick_delay - creates a delay of the appropriate number of Systicks (happens every 1 ms)
-void systick_delay(uint32_t delayTicks) {
-    uint32_t currentTicks;
-
-    currentTicks = msTicks;    //NOTE: read current tick counter
-    // Now loop until required number of ticks passes.
-    while ((msTicks - currentTicks) < delayTicks);
-}
 
 
 
@@ -53,8 +34,8 @@ int main(void) {
     // Setup SysTick Timer to interrupt at 1 msec intervals
     SysTick_Config(CGU_GetPCLKFrequency(CGU_PERIPHERAL_M4CORE)/1000);
 
-    GPIO_SetDir(0, 1<<8, 1);
-    GPIO_ClearValue(0, 1<<8);
+    GPIO_SetDir(0, 1 << 8, 1);
+    GPIO_ClearValue(0, 1 << 8);
 
 
     // Start M0APP slave processor
@@ -73,11 +54,12 @@ int main(void) {
     // Enter an infinite loop, just incrementing a counter
     while(1) {
         systick_delay(500);
-        GPIO_SetValue(0, 1<<8);
+        GPIO_SetValue(0, 1 << 8);
         systick_delay(1000);
-        GPIO_ClearValue(0, 1<<8);
+        GPIO_ClearValue(0, 1 << 8);
 
         i++ ;
     }
+
     return 0 ;
 }
